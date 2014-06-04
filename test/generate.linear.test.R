d7fdbbc4a5ec6e896cddc47c7dda22c407601af8
GuuD WU <guuuuud@gmail.com>
2014-06-04 15:57:33 -0400

    First version of "generate.linear.R"
    Now it can produce a block-diagonal system.
    
    Add package "permute" in "DESCRIPTION".
    
    Create "generate.linear.test.R".

---
+++ test/generate.linear.test.R
@@ -0,0 +1,48 @@
+directory <- '~/program/rpeode/package/R/'
+
+source( paste(directory,'generate.linear.R',sep='') )
+
+# Parameter
+set.seed(0)
+dimension <- as.integer(5)
+time_point <- seq ( 0 , 1 , length.out=11 ) [-1]
+
+# Generate model
+object <-
+  generate.linear (
+    dimension
+    , time_point
+#    , sanitycheck = TRUE
+  )
+
+# Check curve with "ode" function from "deSolve" package.
+linODE <- function ( time , state , pars )
+{
+  res <- pars[[1]] %*% state
+  if ( ! is.null ( pars[[2]] ) )
+    res <- res + pars[[2]]
+  return ( list(res) )
+}
+
+library('deSolve')
+add0 <- time_point[1]!=0
+if ( add0 )
+{
+  time_point <- c ( 0 , time_point )
+}
+ode_res <- deSolve::ode (
+  object$initial
+  , time_point
+  , linODE
+  , list ( object$linear , object$constant )
+)
+if ( add0 )
+{
+  ode_res <- ode_res[-1,]
+}
+
+difference <- ode_res[,-1] - object$data[,-1]
+rss <- norm ( difference , 'F' )
+cat('rss: ')
+cat(rss)
+cat('\n')
